a90a6edf43bd81b1193708758c9ffc70a75f0294
cucuso <edenn001@gmail.com>
2024-04-05 00:26:06 -0400

    improvements

--- js/main.js
+++ js/main.js
@@ -39,10 +39,7 @@ document.onmouseout = (event) => {
 function copyToClipboard() {
     // Get the text field
     var copyText = document.getElementById("ca");
-    console.log(copyText.innerText)
-  
     // Select the text field
-     // Copy the text inside the text field
+    // Copy the text inside the text field
     navigator.clipboard.writeText(copyText.innerText);
-  
-  }
+}

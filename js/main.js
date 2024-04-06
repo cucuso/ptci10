1921de3d44704ceaf029e81e2c0c0823acbc1ce7
cucuso <edenn001@gmail.com>
2024-04-06 15:01:51 -0400

    fixed site

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
 var eyeBall = document.querySelector('#eyeBall');
 var eyeBall2 = document.querySelector('#eyeBall2');
 
+
+window.addEventListener("resize", reportWindowSize);
+
+
+function reportWindowSize() {
+    if (window.innerWidth < 768) {
+        eyeBall.style.display = 'none';
+        eyeBall2.style.display = 'none';
+    } else {
+        eyeBall.style.display = '';
+        eyeBall2.style.display = '';
+    }
+}
+
+
+
+
+eyeBall.style.display = '';
+eyeBall2.style.display = '';
+
 document.onmousemove = (event) => {
     var x = event.clientX * 100 / window.innerWidth;
     var y = event.clientY * 100 / window.innerHeight;
@@ -16,6 +36,8 @@ document.onmousemove = (event) => {
     x = x + "%";
     y = y + "%";
 
+    console.log(x);
+    console.log(y);
 
     eyeBall.style.transition = "0s";
     eyeBall.style.left = x;
@@ -26,15 +48,6 @@ document.onmousemove = (event) => {
     eyeBall2.style.top = y;
 }
 
-document.onmouseout = (event) => {
-    eyeBall.style.transition = "0.7s";
-    eyeBall.style.left = "90%";
-    eyeBall.style.top = "90%";
-
-    eyeBall2.style.transition = "0.7s";
-    eyeBall2.style.left = "90%";
-    eyeBall2.style.top = "90%";
-}
 
 function copyToClipboard() {
     // Get the text field
